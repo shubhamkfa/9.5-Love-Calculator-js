ddba28cf9b6a9871ce1106fd919f0c8451d9b8da
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-03-11 11:49:28 +0530

    Add files via upload

---
+++ 9.5 LoveCalculator.js
@@ -0,0 +1,13 @@
+// var n =Math.random();
+// n=n*6;
+// n=Math.floor(n)+1
+// console.log(n);
+
+
+prompt("Enter your Name? ");
+prompt("Enter Your Loved one's Name? ");
+
+var score=Math.random();
+score=score*100;
+score=Math.floor(score)+1;
+alert("Your Love score is " + score+"%");
